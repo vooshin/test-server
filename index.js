2e0380737ae6ff77a09f83efc03302b5e5497147
shantanuVoosh <shantanu@voosh.in>
2023-04-27 01:43:09 +0530

    basic change

--- index.js
+++ index.js
@@ -15,3 +15,15 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+const x = {
+  km1: {
+    url: "https://ec2-3-109-132-198.ap-south-1.compute.amazonaws.com/",
+  },
+  km2: {
+    url: "https://ec2-13-235-135-43.ap-south-1.compute.amazonaws.com/",
+  },
+  km3: {
+    url: "https://ec2-3-110-151-38.ap-south-1.compute.amazonaws.com/",
+  },
+};
